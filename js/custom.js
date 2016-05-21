0b1a3f6a28cd62f9d3a9882641742c436ddca962
Eduardo Telaya <luis.eduardo.telaya@gmail.com>
2016-05-21 16:45:25 -0500

    Add changes

--- js/custom.js
+++ js/custom.js
@@ -12,19 +12,18 @@ $( document ).ready(function() {
         var json = $.parseJSON(data);
         //now json variable contains data in json format
         //let's display a few items
-        var contador = 0;
+        var contador_vacas = 0;
+        var contador_toros = 0;
+        
         jQuery.each(json, function(key, ganado) {
           ganado.tipo = 'vaca';
           ganado.tipo_color = 'red';
-          contador++;
-          if (contador == 20) {
-            return false;
-          }
+          
           if (ganado.sexo == 1) {
             ganado.tipo = 'toro';
             ganado.tipo_color = 'navy';
+            contador_toros ++;
           }
-          
 
           newItem = ('<div class="element-item col-md-4 ' + ganado.tipo +'">'+
                     '<figure class="snip1174 ' + ganado.tipo_color + ' col-md-4">' +
@@ -35,13 +34,13 @@ $( document ).ready(function() {
                         '<span class="genotipo">Genotipo: ' + ganado.genotipo + '</span><br>' +
                         '<span class="calCarne">Cal. Carne:' + ganado.calCarne + '</span><br>' +
                         '<span class="calLeche">Cal. Leche: ' + ganado.calLeche + '</span><br>' +
-                        '<a href="/simular.html">Simular</a>' +
+                        '<a href="/simular.html" target="_blank">Simular</a>' +
                       '</figcaption>'+
                     '</figure>'+
                 '</div>');
-          $('.grid').append(newItem);
+          $('.grid').append(newItem);          
         });
-                    // init Isotope
+      // init Isotope
       $('.grid').isotope({
         itemSelector: '.element-item',
         layoutMode: 'fitRows'
@@ -70,5 +69,22 @@ $( document ).ready(function() {
       $buttonGroup.find('.is-checked').removeClass('is-checked');
       $( this ).addClass('is-checked');
     });
+  });
+  
+  $('a.optimizar-cruze').click(function(event) {
+    event.preventDefault();
+    var contador = 0;
+    var iso_shuffle = function() {
+      $('.grid').isotope('shuffle');
+      contador++;
+      if (contador > 10) {
+        window.location.href = "/resultado.html";
+      }
+      console.log(contador);
+    };
+    setInterval(iso_shuffle, 1000);
+
+    console.log('sell...');
+    
   });
 });
