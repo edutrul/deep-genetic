122d860a97565505143c1f36642406f9d0f0aa74
Eduardo Telaya <luis.eduardo.telaya@gmail.com>
2016-05-21 14:16:38 -0500

    Update

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,24 @@
 // external js: isotope.pkgd.js
 $( document ).ready(function() {
 
-  console.log('hello');
-  console.log($('body'));
+  $.ajax({
+    url: "data.json",
+    //force to handle it as text
+    dataType: "text",
+    success: function(data) {
+
+        //data downloaded so we call parseJSON function 
+        //and pass downloaded data
+        var json = $.parseJSON(data);
+        //now json variable contains data in json format
+        //let's display a few items
+        console.log(json);
+        
+        //$('#results').html('Plugin name: ' + json.name + '<br />Author: ' + json.author.name);
+    }
+  });
+
+
   // init Isotope
   var $grid = $('.grid').isotope({
     itemSelector: '.element-item',
@@ -11,15 +27,18 @@ $( document ).ready(function() {
   // filter functions
 
   // bind filter button click
-  $('.filters-button-group').on( 'click', 'button', function() {
+  $('.deep-genetic-filters').on( 'click', 'a', function(event) {
+    event.preventDefault();
+    console.log('hello');
     var filterValue = $( this ).attr('data-filter');
+    console.log(filterValue);
     // use filterFn if matches value
     $grid.isotope({ filter: filterValue });
   });
   // change is-checked class on buttons
-  $('.button-group').each( function( i, buttonGroup ) {
+  $('.deep-genetic-filters').each( function( i, buttonGroup ) {
     var $buttonGroup = $( buttonGroup );
-    $buttonGroup.on( 'click', 'button', function() {
+    $buttonGroup.on( 'click', 'a', function() {
       $buttonGroup.find('.is-checked').removeClass('is-checked');
       $( this ).addClass('is-checked');
     });
