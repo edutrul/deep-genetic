dc9d1120ae233e4505fddd49183957b77ecc91c7
Eduardo Telaya <luis.eduardo.telaya@gmail.com>
2016-05-21 11:52:30 -0500

    Add changes with isotope

---
+++ js/custom.js
@@ -0,0 +1,27 @@
+// external js: isotope.pkgd.js
+$( document ).ready(function() {
+
+  console.log('hello');
+  console.log($('body'));
+  // init Isotope
+  var $grid = $('.grid').isotope({
+    itemSelector: '.element-item',
+    layoutMode: 'fitRows'
+  });
+  // filter functions
+
+  // bind filter button click
+  $('.filters-button-group').on( 'click', 'button', function() {
+    var filterValue = $( this ).attr('data-filter');
+    // use filterFn if matches value
+    $grid.isotope({ filter: filterValue });
+  });
+  // change is-checked class on buttons
+  $('.button-group').each( function( i, buttonGroup ) {
+    var $buttonGroup = $( buttonGroup );
+    $buttonGroup.on( 'click', 'button', function() {
+      $buttonGroup.find('.is-checked').removeClass('is-checked');
+      $( this ).addClass('is-checked');
+    });
+  });
+});
