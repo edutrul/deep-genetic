8f3badfcb70d7bf6d6f14032a690b286d9c8241b
Eduardo Telaya <luis.eduardo.telaya@gmail.com>
2016-05-21 14:34:25 -0500

    Add change

--- js/custom.js
+++ js/custom.js
@@ -12,8 +12,36 @@ $( document ).ready(function() {
         var json = $.parseJSON(data);
         //now json variable contains data in json format
         //let's display a few items
-        console.log(json);
-        
+        jQuery.each(json, function(key, ganado) {
+          console.log(ganado.nombre);
+          console.log(ganado.calCarne);
+          console.log(ganado.calLeche);
+          console.log(ganado.genoTipo);
+          var tipo = 'vaca';
+          var tipo_color = 'red';
+           
+          if (ganado.sexo == 0) {
+            
+          }
+          else {
+            
+          }
+          console.log(ganado.sexo);
+        });
+
+        $('grid').append('<div class="element-item col-md-4 vacas">'+
+                  '<figure class="snip1174 red col-md-4">' +
+                  '<img src="images/vaca.jpg" alt="vaca" />' +
+                  '  <figcaption>' +
+                      '<h2 class="nombre">Alicia TT</h2>'+
+                      '<br>' +
+                      '<span class="genotipo">Genotipo: g6egh0eb</span><br>' +
+                      '<span class="calCarne">Cal. Carne: 0.492774145202</span><br>' +
+                      '<span class="calLeche">Cal. Leche: 0.680657017953</span><br>' +
+                      '<a href="/simular.html">Simular</a>' +
+                    '</figcaption>'+
+                  '</figure>'+
+              '</div>');
         //$('#results').html('Plugin name: ' + json.name + '<br />Author: ' + json.author.name);
     }
   });
