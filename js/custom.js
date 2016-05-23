0528bcd52724868fc20067b1a84e7aa5613fea45
Eduardo Telaya <luis.eduardo.telaya@gmail.com>
2016-05-22 23:47:45 -0500

    Custom name

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,23 @@
 // external js: isotope.pkgd.js
 $( document ).ready(function() {
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
 
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+};
+
+ var granja_nombre = getUrlParameter('granja');
+ console.log(granja_nombre);
+ $('.navbar-brand').html('GRANJA DE ' + granja_nombre);
   $.ajax({
     url: "data.json",
     //force to handle it as text
